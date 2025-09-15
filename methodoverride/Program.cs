4561e6d25ad9420a741975909c0c75da12c4187c
Semir Zahirovic <semir_guzonja@hotmail.se>
2025-09-15 15:02:14 +0200

    Add project files.

---
+++ methodoverride/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace methodoverride
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
