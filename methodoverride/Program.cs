fbab344a4b01bad26c87cc2155305585b3dfe058
Semir Zahirovic <semir_guzonja@hotmail.se>
2025-09-17 11:30:13 +0200

    klar

--- methodoverride/Program.cs
+++ methodoverride/Program.cs
@@ -7,6 +7,24 @@
             Console.WriteLine(FormatName("Anna", "Svensson"));
             Console.WriteLine(FormatName("Anna", "Maria", "Svensson"));
             Console.WriteLine(FormatName("Anna", "Maria", "Svensson", true));
+
+        }
+        static string FormatName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+        static string FormatName(string firstName, string middleName, string lastName)
+        {
+            return $"{firstName} {middleName} {lastName}";
+        }
+        static string FormatName(string firstName, string middleName, string lastName, bool uppercase)
+        {
+            string fullName = $"{firstName} {middleName} {lastName}";
+            if (uppercase)
+            {
+                return fullName.ToUpper();
+            }
+            return fullName;
         }
     }
 }
